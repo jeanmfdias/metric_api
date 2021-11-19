1df8cabe37c2aa558c2d04bc0b57419de5fcf5ee
jeanmfdias <jeanmfdias@gmail.com>
2021-11-18 21:58:27 -0300

    added connection with mysql to create logs

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const connection = require('./configs/database');
 
 const app = express();
 
+connection.connect(err => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('successful db connection');
+
+        app.listen(3000,  () => console.log('Server running'));
+    }
+});
+
+app.use(bodyParser.json());
+
 app.get('/v1', (req, res) => {
-    res.json({
+    return res.json({
         "data": "version 1.0.0 | metric api"
     });
 });
 
-app.listen(3000,  () => console.log('Server running'));
+app.post('/v1/convert', (req, res) => {
+    return res.json({
+        "status": "success",
+        "data": req.body
+    });
+});
