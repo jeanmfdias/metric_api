e43b74a113b5a8a03eb9f2c16ce14414440cd5b5
jeanmfdias <jeanmfdias@gmail.com>
2021-11-22 09:00:11 -0300

    added required validation

--- services/conversion_service.js
+++ services/conversion_service.js
@@ -2,8 +2,26 @@ async function validate(body) {
     const value_valid = typeof body.value === 'integer' || typeof body.value === 'number';
     const unit_in_valid = typeof body.unit_in === 'string';
     const unit_out_valid = typeof body.unit_out === 'string';
+    const value_required = body.value !== undefined && body.value !== "";
+    const unit_in_required = body.unit_in !== undefined && body.unit_in !== "";
+    const unit_out_required = body.unit_out !== undefined && body.unit_out !== "";
 
     const validations = [
+        {
+            name: 'value',
+            valid: value_required,
+            message: 'value is required'
+        },
+        {
+            name: 'unit_in',
+            valid: unit_in_required,
+            message: 'unit_in is required'
+        },
+        {
+            name: 'unit_out',
+            valid: unit_out_required,
+            message: 'unit_out is required'
+        },
         {
             name: 'value',
             valid: value_valid,
